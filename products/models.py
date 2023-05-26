4f2080c379e990391f6c144371298b4070ae6195
mahdihasan2005 <mahdihasan2005@disroot.org>
2023-05-26 19:21:31 +0600

    Update db.sqlite3, models.cpython-310.pyc, views.cpython-310.pyc, and 8 more files

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=31)
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=4047)
